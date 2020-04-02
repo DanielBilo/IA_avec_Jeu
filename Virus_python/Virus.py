cce9ec81c34863bc4f3060edf1bf8ad0ddd955bc
DanielBilo <58951246+DanielBilo@users.noreply.github.com>
2020-04-02 19:42:32 -0400

    Add files via upload

---
+++ Virus_python/Virus.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+'''ecran = pygame.display.set_mode([1000,1000])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+'''
